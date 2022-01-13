b9c8cfe4ca47507a0a8c8fa74c7ba26a19b28e5d
Paul Gavrila <paul@madball.ro>
2022-01-13 13:52:00 +0200

    ubuntu - add vue client

---
+++ vue-jsclient/vue.config.js
@@ -0,0 +1,16 @@
+const dotenv = require('dotenv');
+const dotenvExpand = require('dotenv-expand');
+var myEnv = dotenv.config({path: '../.env'});
+dotenvExpand(myEnv);
+
+module.exports = {
+  // outputDir: 'glp',
+  filenameHashing: false,
+  indexPath: 'xxx.welcome.blade.php',
+  devServer: {
+     // host: 'https://worktest.client'
+	 proxy: 'https://finalf.badmintonclub.ro.mydev'
+
+  },
+  configureWebpack: {}
+}
