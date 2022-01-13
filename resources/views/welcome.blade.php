a8954ca804cf753f1e76fa0caf1256151566fb5e
GLP <gavrilapaul@gmail.com>
2022-01-13 22:52:42 +0200

    windows: start app

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,6 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width,initial-scale=1">
-        <link rel="icon" href="img/favicon.ico">
         <title>vue-client</title>
         <link href="{{ asset(mix('css/app.css')) }}" type="text/css" rel="stylesheet" />
         <meta name="csrf-token" value="{{ csrf_token() }}" />
@@ -13,7 +12,7 @@
     <body>
         <noscript><strong>We're sorry but vue-client doesn't work properly without JavaScript enabled. Please enable it to continue.</strong></noscript>
             <div id="app"></div>
+            <script src="{{ asset(mix('js/chunk-vendors.js')) }}" type="text/javascript"></script>
             <script src="{{ asset(mix('js/app.js')) }}" type="text/javascript"></script>
-
     </body>
 </html>
