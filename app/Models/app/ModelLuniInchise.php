67ffd7362688e7101ec50759c5e5b76d1e4c2df5
GLP <gavrilapaul@gmail.com>
2022-02-27 17:08:54 +0200

    windows: tabela luni inchise

---
+++ app/Models/app/ModelLuniInchise.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models\app;
+use App\allClass\helpers\MyHelp;
+use App\allClass\helpers\MyModel;
+use Illuminate\Support\Facades\DB;
+
+class ModelLuniInchise extends MyModel {
+    public function __construct($idAvocat, $idUser)
+    {
+        parent::__construct($idAvocat, $idUser);
+        $this->tableName = 't_s_luni_inchise';
+    }
+
+    public function selectEntity($id){
+        $rezult = DB::select(
+            "
+               select   
+                    id,
+                    id_avocat,
+                    nLuna,
+                    nAn,
+                    inchisa,
+                    created,
+                    last_update,
+                    id_user
+                FROM t_s_luni_inchise
+                 where id = ?;",
+            [$id]
+        );
+        return $rezult;
+    }
+}
---
+++ resources/sqinstaller/trasfer data base/014_t_s_luni_inchise.sql
@@ -0,0 +1,12 @@
+CREATE TABLE badminto_finalf.t_s_luni_inchise LIKE avban.t_s_luni_inchise;
+ALTER TABLE badminto_finalf.t_s_luni_inchise CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+INSERT INTO badminto_finalf.t_s_luni_inchise SELECT * from avban.t_s_luni_inchise;
+ALTER TABLE badminto_finalf.t_s_luni_inchise CHANGE COLUMN id_luna id INT UNSIGNED NOT NULL ;
+ALTER TABLE badminto_finalf.t_s_luni_inchise
+    CHANGE COLUMN id id INT UNSIGNED NOT NULL AUTO_INCREMENT ,
+    ADD UNIQUE INDEX uix_id (id ASC) VISIBLE;
+ALTER TABLE badminto_finalf.t_s_luni_inchise ADD COLUMN created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP;
+ALTER TABLE badminto_finalf.t_s_luni_inchise ADD COLUMN last_update TIMESTAMP NULL AFTER created;
+ALTER TABLE badminto_finalf.t_s_luni_inchise ADD COLUMN id_user INT UNSIGNED NULL;
+update badminto_finalf.t_s_luni_inchise set id_user = 3;
+ALTER TABLE badminto_finalf.t_s_luni_inchise CHANGE COLUMN id_user id_user INT UNSIGNED NULL ;
